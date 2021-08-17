1fb6e7c09802c5494cee39457cd8aaf34ba1ded2
David Huang <zhua687@auckland.uni.nz>
2021-08-17 17:02:25 +1200

    Renaming virtuamode

--- Tree/Assets/MainMenu.cs
+++ Tree/Assets/MainMenu.cs
@@ -6,6 +6,6 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void VirtualMode () {
-        SceneManager.LoadScene("TreeGame");
+        SceneManager.LoadScene("VirtualMode");
     }
 }
