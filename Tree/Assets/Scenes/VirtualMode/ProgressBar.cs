d4a850db9289c18d0d34a1cd3c3d85929697e8f5
David Huang <stonghk@hotmail.com>
2021-09-11 00:59:45 +1200

    Progress bar and leaf button

---
+++ Tree/Assets/Scenes/VirtualMode/ProgressBar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+
+    private Slider slider;
+
+    public float FillSpeed = 0.5f;
+    private float targetProgress = 0;
+
+   private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        IncrementProgress(0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider.value < targetProgress)
+        {
+            slider.value += FillSpeed * Time.deltaTime;
+        }
+    }
+
+    public void IncrementProgress(float newProgress)
+    {
+        slider.value = slider.value + newProgress;
+
+    }
+}
