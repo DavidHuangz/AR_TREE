860c480b0520eca8547750f7c0e81b534c9d3384
JesseZeng2 <jessezeng2@gmail.com>
2021-09-12 02:31:05 +1200

    Only tick when the plan is placed

--- Tree/Assets/Scenes/VirtualMode/PlantLogic/VirtualTime.cs
+++ Tree/Assets/Scenes/VirtualMode/PlantLogic/VirtualTime.cs
@@ -5,29 +5,46 @@ using UnityEngine;
 
 public class VirtualTime : MonoBehaviour
 {
-    public class OnTickEventArgs : EventArgs {
+    public class OnTickEventArgs : EventArgs
+    {
         public int tick;
     }
 
     public static event EventHandler<OnTickEventArgs> OnTick;
 
+    TapToPlaceObject virtualPlant;
+
     private float tickMaxTime = 1f;
+
     private int tick;
+
     private float tickTimer;
 
-    private void Awake() {
+    private void Awake()
+    {
         tick = 0;
     }
 
-    void Update() {
-        tickTimer += Time.deltaTime;
-        if (tickTimer >= tickMaxTime) {
-            tickTimer -= tickMaxTime;
-            tick++;
-            if (OnTick != null) {
-                OnTick(this, new OnTickEventArgs {tick = tick});
-            }
-         }
+    private void Start()
+    {
+        GameObject interaction = GameObject.Find("Interaction");
+        virtualPlant = interaction.GetComponent<TapToPlaceObject>();
     }
 
+    void Update()
+    {
+        if (virtualPlant.seedlingPlaced())
+        {
+            tickTimer += Time.deltaTime;
+            if (tickTimer >= tickMaxTime)
+            {
+                tickTimer -= tickMaxTime;
+                tick++;
+                if (OnTick != null)
+                {
+                    OnTick(this, new OnTickEventArgs { tick = tick });
+                }
+            }
+        }
+    }
 }
