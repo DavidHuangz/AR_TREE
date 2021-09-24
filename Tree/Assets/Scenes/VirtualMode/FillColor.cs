1f6e87d227744df3558e771e66f82e8e6e91a2e4
David Huang <stonghk@hotmail.com>
2021-09-24 23:35:19 +1200

    Ui format and fill finished

--- Tree/Assets/Scenes/VirtualMode/FillColor.cs
+++ Tree/Assets/Scenes/VirtualMode/FillColor.cs
@@ -1,8 +1,6 @@
 using System.Collections;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,9 +10,14 @@ public class FillColor : MonoBehaviour
 
     public Image slider1Fill; //connected the Image Fill from the slider
 
+    // Access other classes
+    private VirtualHandler vh;
+
     // Start is called before the first frame update
     void Start()
     {
+        GameObject mainCanvas = GameObject.Find("UIManager");
+        vh = mainCanvas.GetComponent<VirtualHandler>();
         slider1Fill.color =
             Color.Lerp(Color.green, Color.green, slider1.value / 100);
     }
@@ -22,5 +25,25 @@ public class FillColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        growthStatus();
+    }
+
+    private void growthStatus()
+    {
+        int waterStatus = (int) vh.getWater();
+        int nutrientStatus = (int) vh.getNutrient();
+
+        if (nutrientStatus < 50 || waterStatus < 50)
+        {
+            // no growth
+            slider1Fill.color =
+                Color.Lerp(Color.gray, Color.gray, slider1.value / 100);
+        }
+        else
+        {
+            // Growth
+            slider1Fill.color =
+                Color.Lerp(Color.green, Color.green, slider1.value / 100);
+        }
     }
 }
